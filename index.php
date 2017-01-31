7c4c825e1bf951cf2548391cb0310d578cb82c00
Alexandra Stoica <alexandrasto95@gmail.com>
2017-01-31 15:02:13 +0000

    add index file

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="UTF-8">
+      <title>Cats Expert System</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+      <link href="css/reset.min.css" rel="stylesheet" type="text/css">
+      <link href="css/style.css" rel="stylesheet" type="text/css">
+
+      <!--CatSVG-->
+      <!--source: http://codepen.io/johanmouchet/pen/OXxvqr -->
+      <link href="css/cat.css" rel="stylesheet">
+
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+  </head>
+
+  <body>
+    <div class="cat">
+    	<div class="ear ear--left"></div>
+    	<div class="ear ear--right"></div>
+    	<div class="face">
+    		<div class="eye eye--left">
+    			<div class="eye-pupil"></div>
+    		</div>
+    		<div class="eye eye--right">
+    			<div class="eye-pupil"></div>
+    		</div>
+    		<div class="muzzle"></div>
+    	</div>
+    </div>
+    <h1>Cats Expert System</h1>
+    <a href="pages/expert.php"><button class="btn-cat">Begin</button></a>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+     <!-- Include all compiled plugins (below), or include individual files as needed -->
+     <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
